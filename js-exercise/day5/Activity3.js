f730d035e5d8156961184066dc63d3eb33486496
ishaan khandelwal <104590665+codeishaan@users.noreply.github.com>
2024-07-19 23:53:53 +0530

    day5

--- js-exercise/day5/Activity3.js
+++ js-exercise/day5/Activity3.js
@@ -5,3 +5,11 @@ console.log(sumOf(10,11));
 
 //task6 
 
+let character = (str,char) => str.includes(char);
+const string1 ="hello world";
+const charr ="o";
+
+const result = character(string1,charr);
+console.log(result)
+
+
---
+++ js-exercise/day5/Activity4.js
@@ -0,0 +1,17 @@
+//task 7 
+let p2 =5;
+let product = (p1,p2)=> 
+    {
+        return p1*p2;
+
+    }
+    console.log(product(6,p2));
+
+    //task 8 
+
+    let age = 21;
+    let name = (str,age)=>
+    {
+        console.log(`${str},${age}`)
+    }
+    name("ishaan",age)
