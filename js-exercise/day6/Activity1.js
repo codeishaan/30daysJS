10528f6a4bf419a7e232b702493fc0f28b35f608
ishaan khandelwal <104590665+codeishaan@users.noreply.github.com>
2024-07-20 23:27:46 +0530

    day6

---
+++ js-exercise/day6/Activity1.js
@@ -0,0 +1,6 @@
+//task 1
+let arr =[1,2,3,4,5]
+console.log(arr);
+
+//task 2 
+console.log(arr[0],arr[arr.length-1]);
---
+++ js-exercise/day6/Activity2.js
@@ -0,0 +1,17 @@
+//task 3 
+let arr = [1,2,3,45,5];
+arr.push(7);
+console.log(arr);
+
+//task 4 
+arr.pop()
+console.log(arr)
+
+//task 5 
+arr.shift();
+console.log(arr)
+
+//task 6 
+arr.unshift(9);
+console.log(arr)
+
