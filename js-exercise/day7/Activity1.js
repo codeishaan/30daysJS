52e28321f81b709e12ca135bd14ac26dd20f08dd
ishaan khandelwal <104590665+codeishaan@users.noreply.github.com>
2024-07-21 23:51:03 +0530

    day7

---
+++ js-exercise/day7/Activity1.js
@@ -0,0 +1,13 @@
+//task1
+
+let book ={
+    Title:"the empire",
+    Author:"Eogjs",
+    Year:"2002"
+}
+console.log(book)
+
+//task2 
+
+console.log(book.Title)
+console.log(book.Author)
---
+++ js-exercise/day7/Activity2.js
@@ -0,0 +1,15 @@
+//task 3 
+const books ={
+    Title :"Ansdn",
+    Author: "fjir",
+
+    getSummary: function(){
+        return `${this.Title}, ${this.Author}`;
+    }
+
+};
+
+const BookAuthor = books.getSummary();
+console.log(BookAuthor)
+
+//task 4 
