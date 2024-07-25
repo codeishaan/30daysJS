17fc0c374dd88aaa4fc50b509222267ddec5ab3d
ishaan khandelwal <104590665+codeishaan@users.noreply.github.com>
2024-07-25 08:48:37 +0530

    day9

---
+++ js-exercise/day9-DomManipulation/Activity1.js
@@ -0,0 +1,14 @@
+//task 1 
+const para = document.getElementById('paragraph');
+para.textContent = 'no hello world';
+
+
+
+//task 2
+
+const explain = document.getElementsByClassName('explaination');
+explain[0].style.backgroundColor = 'red';
+
+
+
+
---
+++ js-exercise/day9-DomManipulation/Activity2.js
@@ -0,0 +1,17 @@
+
+//task 3 
+const newElement = document.createElement('div')
+newElement.textContent ='yo yo honey singh';
+document.body.appendChild(newElement);
+
+//task 4 
+
+const ull = document.getElementById('unorder');
+
+
+const newLiElement = document.createElement('li');
+
+newLiElement.textContent = 'never see u again';
+
+ull.appendChild(newLiElement);
+
